103c80dcb8e9abc6c29867c28d914c508482b2f9
wilderOliveira92 <wilder.oliveira92@gmail.com>
2020-02-03 12:36:20 -0300

    migration correct

--- src/database/migrations/20200131111154-create-users.js
+++ src/database/migrations/20200131111154-create-users.js
@@ -7,7 +7,7 @@ module.exports = {
                 type: Sequelize.INTEGER,
                 allowNull: false,
                 autoIncrement: true,
-                primaruKey: true,
+                primaryKey: true,
             },
             name: {
                 type: Sequelize.STRING,
