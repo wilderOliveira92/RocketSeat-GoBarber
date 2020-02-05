42e41701526b8b3601dd56436df89b8fb977735c
wilderOliveira92 <wilder.oliveira92@gmail.com>
2020-02-05 11:00:10 -0300

    modified

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,29 +1,30 @@
-import User from '../models/User';
-import jwt from 'jsonwebtoken';
-import authConfig from '../../config/auth';
-import * as Yup from 'yup';
+import User from "../models/User";
+import jwt from "jsonwebtoken";
+import authConfig from "../../config/auth";
+import * as Yup from "yup";
 
 class SessionControler {
     async store(req, res) {
-
         const schema = Yup.object().shape({
-            email: Yup.string().email().required(),
-            password: Yup.string().required(),
-        })
+            email: Yup.string()
+                .email()
+                .required(),
+            password: Yup.string().required()
+        });
 
         if (!(await schema.isValid(req.body))) {
-            return res.status(400).json({ error: 'Validation fails' });
+            return res.status(400).json({ error: "Validation fails" });
         }
 
         const { email, password } = req.body;
 
         const user = await User.findOne({ where: { email } });
         if (!user) {
-            res.status(401).json({ error: 'User not found' });
+            res.status(401).json({ error: "User not found" });
         }
 
         if (!(await user.checkPassword(password))) {
-            return res.status(401).json({ error: 'password does not match' });
+            return res.status(401).json({ error: "password does not match" });
         }
 
         const { id, name } = user;
@@ -31,13 +32,12 @@ class SessionControler {
             user: {
                 id,
                 name,
-                email,
+                email
             },
             token: jwt.sign({ id }, authConfig.secret, {
-                expiresIn: authConfig.expiresIn,
-            }),
-        })
-
+                expiresIn: authConfig.expiresIn
+            })
+        });
     }
 }
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,12 @@
 module.exports = {
-    dialect: 'postgres',
-    host: '192.168.99.100',
-    username: 'postgres',
-    password: 'docker',
-    database: 'gobarber',
+    dialect: "postgres",
+    host: "192.168.99.100",
+    username: "postgres",
+    password: "docker",
+    database: "gobarber",
     define: {
         timestamps: true,
         underscored: true,
-        underscoredAll: true,
+        underscoredAll: true
     }
 };
